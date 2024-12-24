20b5abd82b1687393f04846825068520f3d5c400
stacks-of-data <coder64bit@gmail.com>
2024-12-25 02:24:58 +0300

    header fix and exec_process malloc protection

--- src/execute_process.c
+++ src/execute_process.c
@@ -3,35 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   execute_process.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bismail <bismail@student.42.fr>            +#+  +:+       +#+        */
+/*   By: coderx64 <coderx64@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:01:23 by bismail           #+#    #+#             */
-/*   Updated: 2024/12/16 14:31:20 by bismail          ###   ########.fr       */
+/*   Updated: 2024/12/25 01:20:03 by coderx64         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-static ssize_t	separator_counter(t_list *lst)
-{
-	ssize_t	counter;
-	size_t	parenthesis_count;
-
-	parenthesis_count = 0;
-	counter = 0;
-	while (lst)
-	{
-		if (((t_token *)lst->content)->type == OPEN_PARENTHESIS)
-			parenthesis_count++;
-		if (((t_token *)lst->content)->type == CLOSE_PARENTHESIS)
-			parenthesis_count--;
-		else if (((t_token *)lst->content)->type >= AND_OPERATOR
-			&& ((t_token *)lst->content)->type <= PIPE && !parenthesis_count)
-			counter++;
-		lst = lst->next;
-	}
-	return (counter);
-}
 
 // static t_list	*find_middle_sep(t_list *lst)
 // {
@@ -79,6 +58,36 @@ static ssize_t	separator_counter(t_list *lst)
 // 	}
 // 	return (1);
 // }
+
+// The execution process will act as a tree,
+// 	as every line parsed will be divided into Segaments.
+
+// 1- We loop across all Tokens to find the middle separator.
+// 2- Then we shall get rerun the function from the first Separator until Null and the second Separator until Null.
+// 3- Now each command would be divided into 2 halfs,
+// 	which will have to do the same function over and over again until.
+
+static ssize_t	main_separators_counter(t_list *lst)
+{
+	ssize_t	counter;
+	size_t	parenthesis_count;
+
+	parenthesis_count = 0;
+	counter = 0;
+	while (lst)
+	{
+		if (((t_token *)lst->content)->type == OPEN_PARENTHESIS)
+			parenthesis_count++;
+		if (((t_token *)lst->content)->type == CLOSE_PARENTHESIS)
+			parenthesis_count--;
+		else if (((t_token *)lst->content)->type >= AND_OPERATOR
+			&& ((t_token *)lst->content)->type <= PIPE && !parenthesis_count)
+			counter++;
+		lst = lst->next;
+	}
+	return (counter);
+}
+
 static int	switch_operations(int	oper_type)
 {
 	if (oper_type == PIPE)
@@ -89,29 +98,39 @@ static int	switch_operations(int	oper_type)
 		return (OPERATION_OR);
 	return (OPERATION_DEFAULT);
 }
-static t_operation **operations_collecter(t_list *lst, ssize_t sep_count)
+
+static int	add_operation_alloc(t_operation	**operations, ssize_t *i)
+{
+	t_operation *temp;
+	
+	temp = malloc(sizeof(t_operation));
+	if (!temp)
+	{
+		while (--(*i) > -1)
+			free(operations[*i]);
+		return (0);
+	}
+	temp->operation_type = switch_operations(-1);
+	operations[*i] = temp;
+	(*i)++;
+	return (1);
+}
+
+static t_operation **main_operations_alloc(t_list *lst, ssize_t sep_count)
 {
 	t_operation  **operations;
-	t_operation *temp;
 	ssize_t		i;
 
 	i = 0;
 	operations = ft_calloc(sizeof(void *), sep_count + 1);
-	temp = malloc(sizeof(t_operation));
-	temp->operation_type = switch_operations(-1);
-	operations[i] = temp;
-	i++;
-	while(lst && i <= sep_count)
+	if (!add_operation_alloc(operations, &i))
+		return (0);
+	while (lst)
 	{
 		if (((t_token *)lst->content)->type >= AND_OPERATOR
 		&& ((t_token *)lst->content)->type <= PIPE)
-		{
-			temp = malloc(sizeof(t_operation));
-			
-			temp->operation_type = switch_operations(((t_token *)lst->content)->type);
-			operations[i] = temp;
-			i++;
-		}
+			if (!add_operation_alloc(operations, &i))
+				return (0);
 		lst = lst->next;
 	}
 	return (operations);
@@ -119,23 +138,12 @@ static t_operation **operations_collecter(t_list *lst, ssize_t sep_count)
 
 int	execute_process(t_minishell *mini)
 {
-	ssize_t	sep_count;
-	t_operation 	**operations; 
-	//t_redirect	*redirections;
-	sep_count =  separator_counter(mini->line_tokens);
-	operations = operations_collecter(mini->line_tokens,sep_count);
+	ssize_t		sep_count;
+	t_operation	**operations; 
+	
+	sep_count = main_separators_counter(mini->line_tokens);
+	operations = main_operations_alloc(mini->line_tokens, sep_count);
 	if (!operations)
 		return (EXIT_FAILURE);
-	printf("%ld %d\n",sep_count, operations[0]->operation_type);
-	printf("%ld %d\n",sep_count, operations[1]->operation_type);
-	//redirections = redirections_collecter(mini, operations);
 	return (EXIT_SUCCESS);
 }
-
-// The execution process will act as a tree,
-// 	as every line parsed will be divided into Segaments.
-
-// 1- We loop across all Tokens to find the middle separator.
-// 2- Then we shall get rerun the function from the first Separator until Null and the second Separator until Null.
-// 3- Now each command would be divided into 2 halfs,
-// 	which will have to do the same function over and over again until.
